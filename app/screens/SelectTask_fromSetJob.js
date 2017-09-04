6979fbfcf60edc35fa47c1e891d51b89f5d24b2c
TIONG <ooitiongshun@gmail.com>
2017-09-04 15:57:37 +1000

    search bar added to all job component

--- app/screens/SelectTask_fromSetJob.js
+++ app/screens/SelectTask_fromSetJob.js
@@ -1,16 +1,24 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {Text, View, TouchableHighlight, ScrollView} from 'react-native';
-import TaskCardSelectable_forEdit from '../components/presentation/TaskCardSelectable_forEdit';
-import XButton from '../components/presentation/XButton';
-import {confirmTask} from '../actions/editSetJob';
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {Text, View, TouchableHighlight, ScrollView, TextInput, TouchableWithoutFeedback, Keyboard} from 'react-native'
+import TaskCardSelectable_forEdit from '../components/presentation/TaskCardSelectable_forEdit'
+import XButton from '../components/presentation/XButton'
+import {confirmTask} from '../actions/editSetJob'
+import {updateSearch} from '../actions/tasks'
+import {resetTaskSearchBar} from '../actions/tasks'
+import store from '../storeConfig'
 
 class SelectTask_fromSetJob extends Component {
   static navigationOptions = ({navigation}) => {
     return {
       title: 'Select Tasks',
-      headerRight: ( <XButton navigation={navigation} />)
+      headerRight: ( <XButton navigation={navigation} />),
+      headerLeft: null
     }
+  }
+
+  componentWillMount() {
+    store.dispatch(resetTaskSearchBar())
   }
 
   render() {
@@ -19,26 +27,36 @@ class SelectTask_fromSetJob extends Component {
     if (this.props.allTasks.length !== 0) {
       hasTask = true
     }
+    let filteredTask = this.props.allTasks.filter(task => {
+      return task.content.toLowerCase().indexOf(this.props.search.toLowerCase()) !== -1
+    })
     return(
-      <View>
         <View>
-          <Text>Search bar goes here</Text>
+          <View>
+              <TextInput
+              style={{height: 40, borderColor: '#f2f3f4', borderWidth: 1}}
+              placeholder={'Search task'}
+              value={this.props.search}
+              maxLength={50}
+              onChangeText={(text) => this.props.updateSearch(text)}
+              autoCorrect={false}
+              />
+          </View>
+          <TouchableHighlight onPress={()=> this.props.confirmTask(this.props.editSetJob,this.props.allSetJobs,navigation)}><View><Text>DONE</Text></View></TouchableHighlight>
+          {
+            hasTask ? (
+              <ScrollView>
+                {
+                  filteredTask.map((task,index) => {
+                    return <TaskCardSelectable_forEdit task={task} key={index} />
+                  })
+                }
+              </ScrollView>
+            ):(
+              <Text>You do not have any task. Simply enter a task and click add</Text>
+            )
+          }
         </View>
-        <TouchableHighlight onPress={()=> this.props.confirmTask(this.props.editSetJob,this.props.allSetJobs,navigation)}><View><Text>DONE</Text></View></TouchableHighlight>
-        {
-          hasTask ? (
-            <ScrollView>
-              {
-                this.props.allTasks.map((task,index) => {
-                  return <TaskCardSelectable_forEdit task={task} key={index} />
-                })
-              }
-            </ScrollView>
-          ):(
-            <Text>You do not have any task. Simply enter a task and click add</Text>
-          )
-        }
-      </View>
     )
   }
 }
@@ -47,15 +65,17 @@ mapStateToProps = (state) => {
   return {
     allTasks: state.tasks.allTasks,
     allSetJobs: state.allSetJobs.jobs,
-    editSetJob: state.editSetJob
+    editSetJob: state.editSetJob,
+    search: state.tasks.search
   }
 }
 
 mapDispatchToProps = (dispatch) => {
   return {
     clicked: () => alert('clicked'),
-    confirmTask: (editSetJob,allSetJobs,navigation) => dispatch(confirmTask(editSetJob,allSetJobs,navigation))
+    confirmTask: (editSetJob,allSetJobs,navigation) => dispatch(confirmTask(editSetJob,allSetJobs,navigation)),
+    updateSearch: (text) => dispatch(updateSearch(text))
   }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(SelectTask_fromSetJob);
+export default connect(mapStateToProps,mapDispatchToProps)(SelectTask_fromSetJob)
--- app/screens/SelectTemplate.js
+++ app/screens/SelectTemplate.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-import {Text, View, TouchableHighlight, ScrollView} from 'react-native';
+import {Text, View, TouchableHighlight, ScrollView, TextInput} from 'react-native';
 import {connect} from 'react-redux';
 import XButton from '../components/presentation/XButton';
 import TemplateCard from '../components/presentation/TemplateCard';
 import {updateSelectTemplateScreenKey} from '../actions/newJob';
 import {selectThisTemplate} from '../actions/newJob';
+import {updateSearch} from '../actions/templates';
+import {resetTemplateSearchBar} from '../actions/templates';
 import store from '../storeConfig';
 
 class SelectTemplate extends Component {
@@ -20,17 +22,31 @@ class SelectTemplate extends Component {
     store.dispatch(updateSelectTemplateScreenKey(this.props.navigation.state.key));
   }
 
+  componentWillUnmount() {
+    store.dispatch(resetTemplateSearchBar())
+  }
+
   render() {
     let hasTemplate = false
     if (this.props.templates.length !== 0) {
       hasTemplate = true
     }
-
+    let filteredTemplate = this.props.templates.filter(template => {
+      return template.client.propertyName.toLowerCase().indexOf(this.props.search.toLowerCase()) !== -1 ||
+             template.title.toLowerCase().indexOf(this.props.search.toLowerCase()) !== -1
+    })
     return(
       <View>
         <View>
           <View>
-            <Text>Search bar goes here</Text>
+            <TextInput
+            style={{height: 40, borderColor: '#f2f3f4', borderWidth: 1}}
+            placeholder={'Search template'}
+            value={this.props.search}
+            maxLength={50}
+            onChangeText={(text) => this.props.updateSearch(text)}
+            autoCorrect={false}
+            />
           </View>
           <TouchableHighlight onPress={()=>this.props.navigation.navigate('SelectClient')}>
             <View><Text>Create New Job</Text></View>
@@ -39,7 +55,7 @@ class SelectTemplate extends Component {
         <ScrollView>
           {
             hasTemplate ? (
-              this.props.templates.map((template,index) => {
+              filteredTemplate.map((template,index) => {
                 return <View key={index}>
                         <TouchableHighlight onPress={()=> this.props.selectThisTemplate(template,this.props.employeeList,this.props.taskList,this.props.navigation)}>
                           <View>
@@ -60,6 +76,7 @@ class SelectTemplate extends Component {
 
 mapStateToProps = (state) => {
   return {
+    search: state.templates.search,
     templates: state.templates.allTemplates,
     employeeList: state.employees.allEmployees,
     taskList: state.tasks.allTasks
@@ -70,7 +87,8 @@ mapDispatchToProps = (dispatch) => {
   return {
     clicked: () => alert('slsls'),
     updateSelectTemplateScreenKey: (key) => dispatch(updateSelectTemplateScreenKey(key)),
-    selectThisTemplate: (template,employeeList,taskList,navigation) => dispatch(selectThisTemplate(template,employeeList,taskList,navigation))
+    selectThisTemplate: (template,employeeList,taskList,navigation) => dispatch(selectThisTemplate(template,employeeList,taskList,navigation)),
+    updateSearch: (text) => dispatch(updateSearch(text)),
   }
 }
 
